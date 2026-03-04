5dc1e5aa524494e26d81be5a4766d01dceb17455
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-04 23:58:38 +0800

    String Concatenation Exercises.

---
+++ string_concatenation_exercises.py
@@ -0,0 +1,16 @@
+#String Concatenation Exercises.
+
+num1 = 15
+num2 = 7
+num3 = 8
+
+sum = num1+num2+num3
+
+print("The sum of", str(num1)+",", str(num2), "and", str(num3), "is", sum)
+
+wholenumber = num1//num2
+remainder = num1%num2
+
+print(str(num1), "divided by", str(num2),"equals", wholenumber ,"remainder", remainder)
+#output: The sum of 15, 7 and 8 is 30 
+#15 divided by 7 equals 2 remainder 1
