337874dd2700df878a072bcb9bd65989134da8fc
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-04 22:54:02 +0800

    String Concatenation

---
+++ test4.py
@@ -0,0 +1,22 @@
+#String Concatenation.
+
+name1 = "Jez"
+name2 = "reel"
+
+print(name1+name2)
+#output: Jezreel
+
+
+
+fname = "Jezreel"
+lname = "Cruz"
+print(fname + ' ' + lname)
+#output: Jezreel Cruz
+
+
+#basic and easiest way example
+
+fname = "Jezreel"
+lname = "Cruz"
+print(fname, lname)
+#output: Jezreel James
