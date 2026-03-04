5c49fabe65b590b89943211de9e10f0ac5384da0
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-04 18:19:08 +0800

    Test2 - add variable and values.

---
+++ test2.py
@@ -0,0 +1,14 @@
+#add variables and vlaues.
+
+
+name = "Jezreel"
+age = 22
+isStudent = True
+#as you can see there is name, age, isStudent. that's what you called(variable names)
+
+
+#let's print variables.
+
+#print(name, age, isStudent)
+
+print('name:', name, 'age:', age, 'is Student:', isStudent)
