1e2a89c6974cde79794a5c56e6722ee810591530
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-05 15:31:22 +0800

    Getting User Inputs Using The Input() Function.

---
+++ test6.py
@@ -0,0 +1,8 @@
+#Getting User Inputs Using The Input() Function
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+print("Your full name is: " + fname + " " + lname)
+#output: Enter your first name: Jezreel
+#Enter your last name: Cruz
+#Your full name is: Jezreel Cruz
