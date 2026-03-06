41fa3edcc5a64875a9a15109926d39cc965e2a8e
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-06 21:32:12 +0800

    Numeric Operator Exercises - How old are you computation.

---
+++ numeric_operator_exercises_2.py
@@ -0,0 +1,18 @@
+#Numeric Operator Exercises.
+
+
+firstname = input("What is your first name? ")
+lastname = input("What is your last name? ")
+year_of_birth = int(input("What year were you born? "))
+
+age = 2026 - year_of_birth
+
+print("Full Name: "+ firstname + " " + lastname)
+print("Age: " + str(age))
+
+#output: 
+#What is your first name? Jezreel
+#What is your last name? Cruz
+#What year were you born? 2003
+#Full Name: Jezreel Cruz
+#Age: 23
