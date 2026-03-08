0f6ccc4bf984958550998cf17b8a320df9b3edc1
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-09 02:40:44 +0800

    Comparison Operator Exercise 2. - Temperature Checker.

--- comparison_operator_exercise_2.py
+++ comparison_operator_exercise_2.py
@@ -21,3 +21,23 @@ else:
         #output:
         #What's temperature degree?: 37.5
         #37.5, - Your Temperature is not good, not allowed to enter.
+
+
+
+
+#Simpler using If Else only.
+
+#temperature = float(input('What\'s temperature degree?: '))
+
+#passed_degree = 36.9
+
+#if temperature <= passed_degree:
+#    print(str(temperature)+', - Your Temperature is good, allowed to enter.')
+#    #output:
+#    #What's temperature degree?: 35.5
+#    #35.5, - Temperature good, allowed to enter.
+#else:
+#        print(str(temperature)+', - Your Temperature is not good, not allowed to enter.')
+#        #output:
+#        #What's temperature degree?: 37.5
+#        #37.5, - Your Temperature is not good, not allowed to enter.
