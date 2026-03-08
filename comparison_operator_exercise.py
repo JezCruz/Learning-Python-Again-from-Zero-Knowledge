32328081603d34dc2efcaf7c14f6c0d739af938a
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-09 01:16:03 +0800

    Comparison Operators Exercises.

---
+++ comparison_operator_exercise.py
@@ -0,0 +1,9 @@
+#Comparison Operator Exercise.
+
+value1 = int(input('Enter first number: '))
+value2 = int(input('Enter first number: '))
+
+if value1 > value2:
+    print("Mas Mataas! si value1, "+str(value1)+" kay sa kay value2, "+str(value2))
+else:
+    print("Mas Mababa! si value1, "+str(value1)+" kay sa kay value2, "+str(value2))
