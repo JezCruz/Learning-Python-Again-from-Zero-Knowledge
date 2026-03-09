ebb36a54af6a9796373e1edf94e31518a408754a
JezCruz <jezreeljamescruz1@gmail.com>
2026-03-10 01:50:08 +0800

    python test termux - find user using list index.

---
+++ test-termux-3.py
@@ -0,0 +1,17 @@
+#elif statemen - search name that stored on data.
+
+
+
+data = ['james','jezreel','luffy','lisa','joy']
+
+
+
+x = int(input("search id index 0 to 10: "))
+
+
+if x < len(data):
+    print(x, 'found.', data[x])
+else:
+    print(x, 'not found.')
+
+
