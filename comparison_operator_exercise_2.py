8d8816d2fb28257901b780ad7fb880364f75bff5
JezCruz <jezreeljamescruz1@gmail.com>
2026-03-09 23:56:14 +0800

    test termux 2 - enter two same number.

---
+++ comparison_operator_exercise_2.py
@@ -0,0 +1,43 @@
+#Comparison Operator Exercise 2. - Temperature Checker.
+
+#create a python code for temperature testing if less than or equal to 36.9 degrees, pwede pumasok.
+#if more than or equal 37 degrees, bawal pumasok.
+
+
+temperature = float(input('What\'s temperature degree?: '))
+
+passed_degree = 36.9
+failed_degree = 37
+
+
+if temperature <= passed_degree:
+    print(str(temperature)+', - Your Temperature is good, allowed to enter.')
+    #output:
+    #What's temperature degree?: 35.5
+    #35.5, - Temperature good, allowed to enter.
+else:
+    if temperature >= failed_degree:
+        print(str(temperature)+', - Your Temperature is not good, not allowed to enter.')
+        #output:
+        #What's temperature degree?: 37.5
+        #37.5, - Your Temperature is not good, not allowed to enter.
+
+
+
+
+#Simpler using If Else only.
+
+#temperature = float(input('What\'s temperature degree?: '))
+
+#passed_degree = 36.9
+
+#if temperature <= passed_degree:
+#    print(str(temperature)+', - Your Temperature is good, allowed to enter.')
+#    #output:
+#    #What's temperature degree?: 35.5
+#    #35.5, - Temperature good, allowed to enter.
+#else:
+#        print(str(temperature)+', - Your Temperature is not good, not allowed to enter.')
+#        #output:
+#        #What's temperature degree?: 37.5
+#        #37.5, - Your Temperature is not good, not allowed to enter.
---
+++ test-termux-2.py
@@ -0,0 +1,11 @@
+#python test 2 using termux.
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+
+if x == y:
+   print("Correct. Your first and second number is same.")
+else:
+   print("Wrong. your number is not same!")
+
