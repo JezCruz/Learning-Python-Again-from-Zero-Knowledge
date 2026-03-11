9d5fac9586c177d060dda2c5c65a5c9661de21ec
JezCruz <jezreeljamescruz1@gmail.com>
2026-03-11 22:47:01 +0800

    python - while loop.

---
+++ while-loop.py
@@ -0,0 +1,12 @@
+#while loop.
+
+x = 1
+
+while x <= 5:
+    print(x)
+    x += 1
+
+#Explanation:
+#The loop starts with x = 1
+#It runs while x <= 5
+#x += 1 increases the value so the loop eventually stops.
