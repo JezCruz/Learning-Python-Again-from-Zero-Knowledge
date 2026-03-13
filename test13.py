df50ca9ee7757c04c73d90a09a8f5ae0240fec95
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-14 05:46:16 +0800

    Student Intro Program.

---
+++ test13.py
@@ -0,0 +1,33 @@
+# Variables + Input + Output.
+
+
+# Student Intro Program
+
+first_name = input("Enter Your First Name: ")
+last_name = input("Enter Your Last Name: ")
+age = int(input("Enter Your Age: "))
+favorite_subject = input("What is Your Favorite Subject? ")
+dream_job = input("What is Your Dream Job? ")
+school_name = input("Enter Name of School: ")
+section = input("Enter Your Section: ")
+hobby = input("What is Your Hobby? ")
+
+
+full_name = first_name + " " + last_name
+
+
+print("\n")
+print("========================")
+print("     STUDENT PROFILE    ")
+print("========================\n")
+print("Name: "+ first_name + " " + last_name)
+print("Age: "+ str(age))
+print("School Name: "+ school_name)
+print("Section: "+ section)
+print("Hobby: "+ hobby)
+print("Favorite Subject: "+ favorite_subject)
+print("Dream Job: "+ dream_job)
+print("\nWonderful Future is Waiting for you! "+ first_name + ", " + last_name + ".")
+print("May your Dreams come true. so Keep it up!")
+
+
