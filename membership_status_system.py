0ebd7341ced5d6fb8b6cff66c3570fd3465c9996
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-19 05:00:11 +0800

    Membership Status System. - Using match-case.

--- membership_status_system.py
+++ membership_status_system.py
@@ -1,4 +1,4 @@
-# Membership Status System.
+# Membership Status System. - Using match-case.
 
 # Write a program that will display the appropriate seminar fee which is based on the
 # membership status and age entered by the user. Use the information below to complete the program.
@@ -10,41 +10,86 @@
 
 # ** if you input the wrong status then display the message "Input Error".
 
-
+print("""
+===========================================================================
+=    Status       |                Age               |      Seminar Fee   =
+=    S or s       | Club member 60 yrs old and above |          5         =
+=    M or m       | Club member less than 60 yrs old |          10        =
+=    N or n       | None member                      |          20        =
+===========================================================================
+""")
 print("================Membership-Status-System================\n")
 
 status = input('Are you a Member? ').lower()
 age = int(input('How old are you? '))
 
+print('\n')
+match (status, age):
+    case ("s", age) if age >= 60:
+        print('Seminar Fee = 5')
+    case ("m", age) if age < 60:
+        print('Seminar Fee = 10')
+    case ("n", age):
+        print('Seminar Fee = 20')
+    case _:
+        print('Input Error')
 
-if status == 's' and age >= 60:
-    print('Seminar Fee = 5')
-elif status == 'm' and age < 60:
-    print('Seminar Fee = 10')
-elif status == 'n':
-    print('Seminar Fee = 20')
-else:
-    print('Input Error')
+
+
 
 
 
 # Output: "Senior"
+# ===========================================================================
+# =    Status       |                Age               |      Seminar Fee   =
+# =    S or s       | Club member 60 yrs old and above |          5         =
+# =    M or m       | Club member less than 60 yrs old |          10        =
+# =    N or n       | None member                      |          20        =
+# ===========================================================================
+# 
 # ================Membership-Status-System================
-
+# 
 # Are you a Member? S
-# How old are you? 68
+# How old are you? 75
+# 
+# 
 # Seminar Fee = 5
 
-# Output: "Minor"
-# ================Membership-Status-System================
 
+
+ 
+# # Output: "Minor"
+# ===========================================================================
+# =    Status       |                Age               |      Seminar Fee   =
+# =    S or s       | Club member 60 yrs old and above |          5         =
+# =    M or m       | Club member less than 60 yrs old |          10        =
+# =    N or n       | None member                      |          20        =
+# ===========================================================================
+# 
+# ================Membership-Status-System================
+# 
 # Are you a Member? M
-# How old are you? 25
+# How old are you? 22
+# 
+# 
+# 
 # Seminar Fee = 10
 
-# Output: "None Member"
-# ================Membership-Status-System================
 
+
+# Output: "None Member"
+# 
+# ===========================================================================
+# =    Status       |                Age               |      Seminar Fee   =
+# =    S or s       | Club member 60 yrs old and above |          5         =
+# =    M or m       | Club member less than 60 yrs old |          10        =
+# =    N or n       | None member                      |          20        =
+# ===========================================================================
+# 
+# ================Membership-Status-System================
+# 
 # Are you a Member? N
-# How old are you? 62
+# How old are you? 35
+# 
+# 
 # Seminar Fee = 20
