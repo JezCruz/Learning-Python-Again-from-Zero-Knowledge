4e477ada41ec5cd79213086b43fbebf47efa8f28
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-19 13:15:37 +0800

    Test15 - Loops.

---
+++ test15.py
@@ -0,0 +1,34 @@
+# Test15 - Loops.
+
+msg = input('Enter Your Message: ')
+loop = int(input('How Many Loops? '))
+
+for i in range(loop):
+    print(msg)
+
+# Output:
+
+# Enter Your Message: I Love You
+# How Many Loops? 22
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
+# I Love You
