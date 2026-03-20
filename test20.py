c5b018440c3c9f458eaef0244a68518ce0cd76f8
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-20 13:53:30 +0800

    Python Test Games - Number Guessing Game.

---
+++ test20.py
@@ -0,0 +1,21 @@
+# Python Test Games - Number Guessing Game.
+import random
+
+print("\n=== Number Guessing Game ===\n")
+
+secret_number = random.randint(1, 20)
+attempts = 0
+guess = 0
+
+while guess != secret_number:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if guess < secret_number:
+        print("Too low!\n")
+    elif guess > secret_number:
+        print("Too high!\n")
+    else:
+        print(f"Correct! The number is {secret_number}")
+        print(f"You guessed it in {attempts} attempts.")
+        
