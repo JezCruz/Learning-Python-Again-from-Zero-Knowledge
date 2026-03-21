8cbb7a66a83c8067f3ab6a9533e33fd4e40f641f
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-03-21 17:38:34 +0800

    Menu Program

---
+++ menu_program.py
@@ -0,0 +1,48 @@
+# accessing some project in one program, by typing number.
+
+import time
+
+while True:
+
+    try:
+        selection = int(input("Choose Task by Typing Number 0-10: "))
+
+        if selection == 0:
+            print("\\"),time.sleep(1.5), print("| Exiting program... Thank you! |"),time.sleep(2), print("/")
+            break
+        
+        if selection == 1:
+            print("""
+===========================================================  
+=================== Your on Selection 1 ===================
+===========================================================\n""")
+            import coffee_outlet
+
+            print("""\n
+==========================================
+===============Thank You!=================
+==========================================\n""")
+        elif selection == 2:
+            print("""
+===========================================================  
+=================== Your on Selection 2 ===================
+===========================================================\n""")
+            import membership_status_system
+            print("""\n
+==========================================
+===============Thank You!=================
+==========================================\n""")
+        elif selection == 3:
+            print("""
+===========================================================  
+=================== Your on Selection 3 ===================
+===========================================================\n""")
+            import access_system
+            print("""\n
+==========================================
+===============Thank You!=================
+==========================================\n""")
+        else:
+            print("Wrong Selection.")
+    except ValueError:
+        print("Please enter a valid number.")
