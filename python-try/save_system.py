e9106b8c4e8bcdf74dd7789b2335ec407977efe3
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-04-03 02:02:23 +0800

    mini database system - add, read, exit.

--- python-try/save_system.py
+++ python-try/save_system.py
@@ -4,15 +4,76 @@ import os
 
 os.makedirs("python-try/user_data", exist_ok=True)
 
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+while True:
 
-with open("python-try/user_data/data.txt", "a") as f:
-    f.write(f"Name: {name}, Age: {age}\n")
-print("Saved Successfully!")
+    print("""\n+ = = = = User Data = = = = +
 
-# Output:
+    Selections:
+    0 = Exit.
+    1 = Input User Data.
+    2 = Read User Data.""")
+    selection = input("\nSelect: ")
+
+    if selection == "1":
+        
+        name = input("Enter your name: ")
+        age = int(input("Enter your age: "))
+
+        with open("python-try/user_data/data.txt", "a") as f:
+            f.write(f"Name: {name}, Age: {age}\n")
+        print("Saved Successfully!")
+
+    elif selection == "2":
+        
+        with open("python-try/user_data/data.txt", "r") as f:
+            print(f.read())
+        print("\nAll Data Printed Successfully!")
+    elif selection == "0":
+        print("Goodbye!")
+        break
+    else:
+        print("\nInvalid Selection.\n")
+
+
+# Output: 
+# + = = = = User Data = = = = +
+# 
+#     Selections:
+#     0 = Exit.
+#     1 = Input User Data.
+#     2 = Read User Data.
+# 
+# Select: 1
+# Enter your name: fenrir
+# Enter your age: 19
+# Saved Successfully!
+# 
+# + = = = = User Data = = = = +
+# 
+#     Selections:
+#     0 = Exit.
+#     1 = Input User Data.
+#     2 = Read User Data.
+# 
+# Select: 2
 # Name: James, Age: 25
 # Name: Jezreel, Age: 23
 # Name: Anger, Age: 36
 # Name: John, Age: 5645
+# Name: Johval, Age: 19
+# Name: mel, Age: 19
+# Name: mav, Age: 19
+# Name: fenrir, Age: 19
+# 
+# 
+# All Data Printed Successfully!
+# 
+# + = = = = User Data = = = = +
+# 
+#     Selections:
+#     0 = Exit.
+#     1 = Input User Data.
+#     2 = Read User Data.
+# 
+# Select: 0
+# Goodbye!
