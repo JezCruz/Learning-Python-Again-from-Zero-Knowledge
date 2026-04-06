34a23a3903a93b3e39e7f54593f6069c226ec391
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-04-06 20:49:27 +0800

    Search and Filter Data - task1, task2, task3.

--- python-try/searching_and_filtering_data/search_and_filter_data.py
+++ python-try/searching_and_filtering_data/search_and_filter_data.py
@@ -7,17 +7,60 @@ file_loc = "python-try/python-json/data-json/users_data.json"
 with open(file_loc, "r")as f:
     users = json.load(f)
 
-search_name = input("Search User: ").strip().title()
+# Task 1 - Search User by exact name.
 
-found = False
+# search_name = input("Search User: ")
 
-for user in users:
-    if user["name"] == search_name:
-        print("\n=-=-=-= User Found: =-=-=-=")
-        print("Name:", user["name"])
-        print("Age:", user["age"])
-        print()
-        found = True
+# found = False
 
-if not found:
-    print("\nUser not found\n")
+# for user in users:
+#     if search_name.strip().title() == user["name"]:
+#         print("\n= = = User Info = = =\n")
+#         print("name:",user["name"])
+#         print("Age:",user["age"])
+#         print()
+#         found = True
+
+# if not found:
+#     print("\nUser not found.\n")
+
+
+#Task 2 - Partial Search.
+
+# search_name = input("Search Name: ").strip().lower()
+
+# found = False
+
+# for user in users:
+#     if search_name in user["name"].lower():
+#         print("\n= = = Users Found = = =\n")
+#         print(user)
+#         print()
+#         found = True
+# if not found:
+#     print("\n= = = User not found. = = =\n")
+
+
+# Task 3 - Filter user by age.
+
+# print("= = = = filter users. = = = =")
+
+# while True:
+#     filter_user = input("Enter age greater-than or equal?: ").strip()
+#     if filter_user.isdigit():
+#         filter_user = int(filter_user)
+#         break
+#     else:
+#         print("Invalid Input.")
+
+# found = False
+
+# for user in users:
+#     if user["age"] >= filter_user:
+#         print("Name:",user["name"])
+#         print("Age:",user["age"])
+#         print()
+#         found = True
+
+# if not found:
+#     print("No User with that age.")
