f07c7a26a5712a4ba73939f367f2130261f96833
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-04-08 02:16:53 +0800

    CRUD system partial commit #2 - adding: update user data.

--- python-try/crud/update_delete_by_name.py
+++ python-try/crud/update_delete_by_name.py
@@ -23,6 +23,38 @@ def read_data():
                print("Age:",user["age"])
                print()
 
+def update_user():
+     
+    select_name = input("Search User name want's to update: ").strip().title()
+
+    found = False
+
+    for user in users:
+        if select_name == user["name"]:
+            print("User Named", select_name, "found!, and able to update.\n")
+        
+            update_name = input("Enter your updated name: ").strip().title()
+
+            while True:
+                update_age = input("Enter your updated age: ").strip()
+                if update_age.isdigit():
+                    update_age = int(update_age)
+                    break
+                else:
+                    print("Invalid age.")         
+
+            user["name"] = update_name
+            user["age"] = update_age
+
+            print("User Updated!")
+            found = True
+            break
+    if not found:
+            print("User not found.")
+
+    with open(file_loc, "w")as f:
+            json.dump(users, f, indent=4)
+
 while True:
     selection = input("enter function: ").strip().lower()
 
@@ -55,3 +87,6 @@ while True:
 
     elif selection == "read data":
          read_data()
+
+    elif selection == "update data":
+         update_user()
