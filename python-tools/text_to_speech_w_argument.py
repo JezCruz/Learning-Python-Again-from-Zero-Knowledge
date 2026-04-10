166ee96d8c9331ee96df6f8f8ec0ddea0577b4eb
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-04-10 23:19:01 +0800

    CLI version with argument - with change speed rate

--- python-tools/text_to_speech_w_argument.py
+++ python-tools/text_to_speech_w_argument.py
@@ -3,8 +3,10 @@ import pyttsx3
 
 parser = argparse.ArgumentParser(description="Simple Text to Speech CLI")
 parser.add_argument("text", help="Text to speak")
+parser.add_argument("--rate", type=int, default=150, help="Speech rate")
 args = parser.parse_args()
 
 engine = pyttsx3.init()
+engine.setProperty("rate", args.rate)
 engine.say(args.text)
 engine.runAndWait()
